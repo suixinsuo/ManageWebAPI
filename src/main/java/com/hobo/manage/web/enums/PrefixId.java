c2482476268305d7fe2bbe5caceee30d372f0668
jpg <850621354@qq.com>
2019-11-17 14:23:49 +0800

    update

--- src/main/java/com/hobo/manage/web/enums/PrefixId.java
+++ src/main/java/com/hobo/manage/web/enums/PrefixId.java
@@ -6,16 +6,16 @@ package com.hobo.manage.web.enums;
 public enum PrefixId {
 //	cache.builderId(PrefixId.USER_ID.getPrefix(), PrefixId.USER_ID.getLength())
 	// 代理商id
-	AGENT_ID("A",6),
-	MERCHAT_ID("M",6),
-	SYS_ID("S",6),
-	LOG_ID("L",12),
+	AGENT_ID("A",1),
+	MERCHAT_ID("M",1),
+	SYS_ID("S",1),
+	LOG_ID("L",1),
 	// 代理商和商家用户id
-	USER_ID("U",6),
+	USER_ID("U",1),
 	// 退款id
-	REFUND_ID("R",6),
+	REFUND_ID("R",1),
 	// 公告id
-	NOTICE_ID("N",6);
+	NOTICE_ID("N",1);
 	// 前缀
 	private String prefix;
 	// 长度
